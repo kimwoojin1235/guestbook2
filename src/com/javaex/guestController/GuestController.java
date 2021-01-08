622d2ae3cb252ab5cc4f3f5cfc78afcfe25822a2
user <user@PC2005221900>
2021-01-08 12:39:45 +0900

    포스트 방식으로 수정

--- src/com/javaex/guestController/GuestController.java
+++ src/com/javaex/guestController/GuestController.java
@@ -65,6 +65,48 @@ public class GuestController extends HttpServlet {
 				
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String action = request.getParameter("action");
+		System.out.println(action);
+		//한글 깨짐 방지용
+		request.setCharacterEncoding("UTF-8");
+		
+		if("list".equals(action)){
+			//System.out.println("리스트 처리");
+			GuestDao gDao = new GuestDao();
+			List<GuestVo> gList= gDao.getList();
+
+			request.setAttribute("gList",gList);
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/addList.jsp"); //jsp파일 위치를 알려줌
+			rd.forward(request, response);
+		}else if ("add".equals(action)) {
+			//System.out.println("등록폼 처리");
+			request.setCharacterEncoding("utf-8");
+			String name = request.getParameter("name");
+			String password = request.getParameter("password");
+			String content = request.getParameter("content");
+				
+			GuestVo guVo =new GuestVo(name,password,content);
+
+			GuestDao guDao = new GuestDao();
+			guDao.guestinsert(guVo);
+			response.sendRedirect("/guestbook2/pbc?action=list");
+		}else if ("delete".equals(action)) {
+			//System.out.println("삭제");
+			RequestDispatcher rd=request.getRequestDispatcher("/WEB-INF/deleteForm.jsp");
+			rd.forward(request, response);
+		}else if ("delete1".equals(action)) {
+			int no=Integer.parseInt(request.getParameter("no"));
+			String password = request.getParameter("password");
+			GuestVo guestVo = new GuestVo(no,password);
+			GuestDao guestDao = new GuestDao();		
+			int er=guestDao.guestdelete(guestVo);
+			if(er==1) {
+			response.sendRedirect("/guestbook2/pbc?action=list");
+			}else if (er==0){
+				RequestDispatcher rd=request.getRequestDispatcher("/WEB-INF/passerror.jsp");
+				rd.forward(request, response);
+			}
+		}
 	}
 
 }
